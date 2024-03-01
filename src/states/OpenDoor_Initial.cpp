b1f6773f147903bdc1c6de28b875fbffe91a64ca
Chenzheng Wang <templarares@163.com>
2024-03-01 19:13:00 +0800

    1.Teleport won't get synced into mc_mujoco; need to make mc_mujoco calls from python; or maybe add something in mc_mujoco?; 2.added init posW from config file, need to implement this in OpenDoor.cpp and OpenDoor_Initial.cpp simultaneously; 3. next step is to make a roughly working demo using relative hand displacement and then convert it to gym

--- src/states/OpenDoor_Initial.cpp
+++ src/states/OpenDoor_Initial.cpp
@@ -2,6 +2,17 @@
 
 #include "../OpenDoor.h"
 
+
+static void SetInitialPose(mc_control::fsm::Controller & ctl, const sva::PTransformd & pose)
+{
+  ctl.robot().posW(pose);
+  ctl.removeContact({"bit_humanoid", "ground", "LeftFoot", "AllGround"});
+  ctl.removeContact({"bit_humanoid", "ground", "RightFoot", "AllGround"});
+  ctl.addContact({"bit_humanoid", "ground", "LeftFoot", "AllGround"});
+  ctl.addContact({"bit_humanoid", "ground", "RightFoot", "AllGround"});
+  //SaveInitialPose(ctl);
+}
+
 void OpenDoor_Initial::configure(const mc_rtc::Configuration & config)
 {
 }
@@ -10,6 +21,20 @@ void OpenDoor_Initial::start(mc_control::fsm::Controller & ctl_)
 {
   auto & ctl = static_cast<OpenDoor &>(ctl_);
   ctl.gui()->addElement({}, mc_rtc::gui::Button("Open door", [this]() { openDoor_ = true; }));
+
+  //sync init pose
+  sva::PTransformd default_pose;
+  {
+    const auto & da = ctl.robot().module().default_attitude();
+    default_pose.translation() << da[4], da[5], da[6];
+    default_pose.rotation() = Eigen::Quaterniond{da[0], da[1], da[2], da[3]}.toRotationMatrix();
+  }
+  if(ctl.robot().posW() == default_pose)
+  {
+    mc_rtc::Configuration init_pose_cfg(static_cast<std::string>(ctl.config()("ETC_DIR")) + "/initial_pose.yaml");
+    ctl.robot().posW(init_pose_cfg("initial_pose"));
+    mc_rtc::log::success("Check if posW is in sync");
+  }
 }
 
 bool OpenDoor_Initial::run(mc_control::fsm::Controller & ctl_)
